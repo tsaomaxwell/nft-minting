96b2bb00a261809d84315a52e8d9c23802977972
tsaomaxwell <68205375+tsaomaxwell@users.noreply.github.com>
2021-09-27 00:29:36 -0700

    basic nft minting setup

--- contracts/MyEpicNFT.sol
+++ contracts/MyEpicNFT.sol
@@ -3,9 +3,26 @@
 pragma solidity ^0.8.0;
 
 import "hardhat/console.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
 
-contract MyEpicNFT {
-    constructor() {
+contract MyEpicNFT is ERC721URIStorage {
+
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIds;
+
+    constructor() ERC721 ("SquareNFT","SQUARE") {
         console.log("This is my NFT contract. whassup!");
+    }
+
+    function makeAnEpicNFT() public {
+        uint256 newItemId = _tokenIds.current();
+
+        _safeMint(msg.sender, newItemId);
+
+        _setTokenURI(newItemId, "https://jsonkeeper.com/b/J6NU");
+        console.log("An NFT w/ ID %s has been minted to %s", newItemId, msg.sender);
+        
+        _tokenIds.increment();
     }
 }
